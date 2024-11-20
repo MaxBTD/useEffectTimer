540c5985c4d445dad46ecdc4b1fc5652e61afb99
5m <konradek982@gmail.com>
2024-11-20 10:59:32 +0100

    timer working

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect} from 'react';
+import { useState } from 'react';
 
 function App() {
+  const[seconds, setSeconds] = useState(0);
+  const[minutes, setMinutes] = useState(0);
+  const[hours, setHours] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      if(seconds < 60){
+        setSeconds(seconds + 1);
+        }else{
+          setSeconds(0);
+          if(minutes < 60){
+            setMinutes(minutes + 1);
+          } else{
+            setMinutes(0);
+            setHours(hours + 1);
+          }
+        }
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, [seconds]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 id="timerH1">{hours}:{minutes < 10 ? "0" : ""}{minutes}:{seconds < 10 ? "0" : ""}{seconds}</h1>
     </div>
   );
 }
